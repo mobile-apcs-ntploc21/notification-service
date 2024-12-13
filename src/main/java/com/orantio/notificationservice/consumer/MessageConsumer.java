bc836edef4acaf748ece8e963b2d4bc3ba59ed2e
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-12-13 11:20:50 +0700

    SE-56: Combine all messages

--- src/main/java/com/orantio/notificationservice/consumer/MessageConsumer.java
+++ src/main/java/com/orantio/notificationservice/consumer/MessageConsumer.java
@@ -1,14 +1,22 @@
 package com.orantio.notificationservice.consumer;
 
 import com.orantio.notificationservice.Constant;
-import com.orantio.notificationservice.dto.MessageDTO;
+import com.orantio.notificationservice.dto.DirectMessageDTO;
+import com.orantio.notificationservice.dto.ServerMessageDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class MessageConsumer {
     @RabbitListener(queues = Constant.MESSAGE_QUEUE)
-    public void consumeMessage(MessageDTO message) {
-        System.out.println("Received message: " + message);
+    public void consumeDirectMessage(DirectMessageDTO message) {
+        log.info("Received direct message: {}", message);
+    }
+
+    @RabbitListener(queues = Constant.MESSAGE_QUEUE)
+    public void consumeServerMessage(ServerMessageDTO message) {
+        log.info("Received server message: {}", message);
     }
 }
