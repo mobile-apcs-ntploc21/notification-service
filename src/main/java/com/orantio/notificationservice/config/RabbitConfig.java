61c62b5dc13aac8624503473a2552228d7927a9c
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-12-17 10:37:54 +0700

    SE-56: Initialize queues if not exist

--- src/main/java/com/orantio/notificationservice/config/RabbitConfig.java
+++ src/main/java/com/orantio/notificationservice/config/RabbitConfig.java
@@ -1,5 +1,10 @@
 package com.orantio.notificationservice.config;
 
+import com.orantio.notificationservice.constant.Constant;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +12,18 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableRabbit
+@RequiredArgsConstructor
 public class RabbitConfig {
+    private final AmqpAdmin amqpAdmin;
+
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @PostConstruct
+    public void initialize() {
+        amqpAdmin.declareQueue(new Queue(Constant.TOPIC_QUEUE, true));
+        amqpAdmin.declareQueue(new Queue(Constant.NOTIFICATION_QUEUE, true));
     }
 }
